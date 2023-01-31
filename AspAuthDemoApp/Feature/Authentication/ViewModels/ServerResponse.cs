7b39e2eb493056a772b35d7af65922a8e2ea96c1
Keith Sewanonda <keith@laboremus.ug>
2023-01-31 12:20:13 +0300

    feat: add auth endpoints

---
+++ AspAuthDemoApp/Feature/Authentication/ViewModels/ServerResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AspAuthDemoApp.Feature.Authentication.ViewModels
+{
+    public class ServerResponse
+    {
+        public string Status { get; set; } = default!;
+
+        public string Message { get; set; } = default!;
+    }
+}
